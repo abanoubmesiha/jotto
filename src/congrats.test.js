ad7b4dc3298a510fec994f2659c64aed666ba4aa
Abanoub Ayoub <abanoub@terkwaz.com>
2020-12-16 23:44:57 +0200

    Check props of component.

--- src/congrats.test.js
+++ src/congrats.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-import { findByTestAttr } from '../test/testUtils';
+import { checkProps, findByTestAttr } from '../test/testUtils';
 import Congrats from './congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
@@ -12,18 +12,21 @@ const setup = (props={}) => {
 }
 
 test('renders without error', ()=>{
-    const wrapper = setup();
+    const wrapper = setup({ success: false });
     const component = findByTestAttr(wrapper, 'component-congrats');
     expect(component.length).toBe(1);
 })
 test('renders no text when `success` prop is false', ()=>{
-    const wrapper = setup({ success: false});
+    const wrapper = setup({ success: false });
     const component = findByTestAttr(wrapper, 'component-congrats');
     expect(component.text()).toBe('');
 })
 test('renders non-empty congrats message when `success` prop is true', ()=>{
-    const wrapper = setup({ success: true});
+    const wrapper = setup({ success: 'true'});
     const message = findByTestAttr(wrapper, 'congrats-message');
     expect(message.text().length).not.toBe(0);
-
+})
+test('does not throw warning with expected props', ()=>{
+    const expectedProps = { success: false };
+    checkProps(Congrats, expectedProps)
 })
