e186f9b54de22132813edd26c53d887a8f0f21f8
Abanoub Ayoub <abanoub@terkwaz.com>
2020-12-31 00:47:55 +0200

    Setup guessWords tests.

---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+
+
+
+describe('guessWord action dispatcher', () => {
+    describe('no guessed words', () => {
+        test('update state correctly from unsuccessful guess', () => {
+
+        })   
+        test('update state correctly from successful guess', () => {
+
+        })   
+    })
+    describe('some guessed words', () => {
+        test('update state correctly from unsuccessful guess', () => {
+
+        })   
+        test('update state correctly from successful guess', () => {
+
+        })   
+    })
+})
---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,3 @@
+export const guessedWords = (state, action) => {
+    return null
+}
