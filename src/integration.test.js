ff3fdea1e1acfb37aa9c812dbd79b7c85c87bf41
Abanoub Ayoub <abanoub@terkwaz.com>
2021-01-12 18:37:58 +0200

    Add secret word reducer.
    * Start testing guessedwords.

--- src/integration.test.js
+++ src/integration.test.js
@@ -1,10 +1,28 @@
-
+import React from 'react'
+import { storeFactory } from '../test/testUtils'
+import { guessWord } from './actions';
 
 
 describe('guessWord action dispatcher', () => {
     describe('no guessed words', () => {
+        let store;
+        const secretWord = 'party'
+        const unsuccessfulGuess = 'train'
+        const initialState = { secretWord };
+        beforeEach(() => {
+            store = storeFactory(initialState)
+        })
         test('update state correctly from unsuccessful guess', () => {
-
+            store.dispatch(guessWord(unsuccessfulGuess));
+            const newState = store.getState()
+            const expectedState = {
+                ...initialState,
+                success: false,
+                guessedWord: [
+                    { guessedWord: unsuccessfulGuess, letterMatchCount: 3 }
+                ]
+            }
+            expect(newState).toEqual(expectedState)
         })   
         test('update state correctly from successful guess', () => {
 
