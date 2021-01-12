763566753a0107a7bd033ef086032dda69a53cd0
Abanoub Ayoub <abanoub@terkwaz.com>
2021-01-12 21:55:11 +0200

    Connect App component and tests

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Congrats from './components/Congrats';
 import GuessedWords from './components/GuessedWords';
 import Input from './components/input';
 import './App.css';
+import { getSecretWord } from './actions'
 
 class App extends Component {
   render(){
+    const { success, guessedWords } = this.props
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
+        <Congrats success={success} />
         <Input />
-        <GuessedWords guessedWords={[
-          { guessedWord: 'train', letterMatchCount: 3 }
-        ]} />
+        <GuessedWords guessedWords={guessedWords} />
       </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = state => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord }
+}
+export default connect(mapStateToProps, { getSecretWord })(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Enzyme,{ shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { storeFactory } from '../test/testUtils';
 import App from './App';
-// import { Provider } from 'react-redux'
 
-test('renders learn react link', () => {
-  // const initialState = { success: false}
-  // const store = storeFactory(initialState)
-  // render(
-  // <Provider store={store}>
-  //   <App />
-  // </Provider>);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+const setup = (state={}) => {
+  const store = storeFactory(state);
+  const wrapper = shallow(<App store={store} />).dive().dive();
+  return wrapper;
+}
+
+describe('redux props', () => {
+  test('has access to `success` state', () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProps = wrapper.instance().props.success;
+    expect(successProps).toBe(success);
+  })
+  test('has access to `guessedWords` state', () => {
+    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProps = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProps).toEqual(guessedWords);
+  })
+  test('has access to `secretWord` state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProps = wrapper.instance().props.secretWord;
+    expect(secretWordProps).toBe(secretWord);
+  })
+  test('has access to `getSecretWord` action creator', () => {
+    const wrapper = setup();
+    const getSecretWordProps = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProps).toBeInstanceOf(Function);
+  })
+})
